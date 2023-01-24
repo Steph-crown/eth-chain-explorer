312abb1d062890f3bb1a18cff6f2c4c44cd7f733
Steph Crown <emmanuelstephen024@gmail.com>
2023-01-24 10:25:58 +0100

    Set up react-hot-toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
+"use client";
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
 
 export default function RootLayout({
   children,
@@ -12,7 +14,18 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        {children}
+        <Toaster
+          toastOptions={{
+            style: {
+              fontSize: "14px",
+              color: "#555",
+              fontWeight: "500",
+            },
+          }}
+        />
+      </body>
     </html>
   );
 }
