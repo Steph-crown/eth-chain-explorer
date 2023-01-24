7801addcfc67ae0e12f4972c471ba7ce57878e01
Steph Crown <emmanuelstephen024@gmail.com>
2023-01-24 08:40:52 +0100

    Create search component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
-import { Inter } from "@next/font/google";
-import styles from "./page.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { Search } from "@/components";
 
 export default function Home() {
-  return <main className={styles.main}>Hi</main>;
+  return (
+    <main>
+      <header>
+        <h1>Ethereum Blockchain Explorer</h1>
+      </header>
+      <Search />
+    </main>
+  );
 }
