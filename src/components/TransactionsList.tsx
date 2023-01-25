c719eaa0cde9ffe605a338b78d99aaf610e49093
Steph Crown <emmanuelstephen024@gmail.com>
2023-01-25 20:54:39 +0100

    Create loader component

--- src/components/TransactionsList.tsx
+++ src/components/TransactionsList.tsx
@@ -2,15 +2,17 @@
 import { TransactionsContext } from "@/contexts";
 import { ITransactionsContext } from "@/contexts/transactionsContext";
 import React, { Context, useContext } from "react";
+import Loader from "./Loader";
 import TransactionCard from "./TransactionCard";
 
 const TransactionsList = () => {
-  const { transactions } = useContext(
+  const { transactions, loadingTransaction } = useContext(
     TransactionsContext as Context<ITransactionsContext>
   );
 
   return (
     <div className="txs-list">
+      {loadingTransaction ? <Loader /> : null}
       {transactions.map((tx, id) => (
         <TransactionCard key={tx.addr + id} {...tx} />
       ))}
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,5 @@
 export { default as Filter } from "./Filter";
+export { default as Loader } from "./Loader";
 export { default as Search } from "./Search";
 export { default as Tag } from "./Tag";
 export { default as TagList } from "./TagList";
